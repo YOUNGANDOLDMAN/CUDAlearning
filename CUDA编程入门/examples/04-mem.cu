72f738d92b912e11c737fc34e0aa4a9dcca4d44e
YOUNGANDOLDMAN <129586545+YOUNGANDOLDMAN@users.noreply.github.com>
2023-05-23 21:29:26 +0800

    Update 04-mem.cu

--- CUDA编程入门/examples/04-mem.cu
+++ CUDA编程入门/examples/04-mem.cu
@@ -2,25 +2,26 @@
 #include <stdio.h>
 #include <cuda.h>
 
-typedef double FLOAT;
+typedef double FLOAT;//定义FLOAT为双精度的
 
 __global__ void sum(FLOAT *x)
 {
-    int tid = threadIdx.x;
+    int tid = threadIdx.x;//获取线程索引，一维数组
 
-    x[tid] += 1;
+    x[tid] += 1;//每个数+1
+    
 }
 
 int main()
 {
-    int N = 32;
-    int nbytes = N * sizeof(FLOAT);
+    int N = 32;//定义数组的元素数目
+    int nbytes = N * sizeof(FLOAT);//确定数组的大小，方便下面申请内存
 
-    FLOAT *dx = NULL, *hx = NULL;
+    FLOAT *dx = NULL, *hx = NULL;//初始化gpu和cpu的内存大小
     int i;
 
     /* allocate GPU mem */
-    cudaMalloc((void **)&dx, nbytes);
+    cudaMalloc((void **)&dx, nbytes);//申请内存
 
     if (dx == NULL) {
         printf("couldn't allocate GPU memory\n");
