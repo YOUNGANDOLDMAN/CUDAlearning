3c5c669a75c4eff60076629f31df9cb5c1a172e0
YOUNGANDOLDMAN <129586545+YOUNGANDOLDMAN@users.noreply.github.com>
2023-05-23 16:11:16 +0800

    Update vec-add.cu

--- CUDA编程入门/examples/vec-add.cu
+++ CUDA编程入门/examples/vec-add.cu
@@ -33,10 +33,10 @@ int main()
     int bs = 256;//一个block里面有256个线程，还是一维
 
     /* 2D grid */
-    int s = ceil(sqrt((N + bs - 1.) / bs));
-    dim3 grid = dim3(s, s);
+    int s = ceil(sqrt((N + bs - 1.) / bs));//确定block的个数
+    dim3 grid = dim3(s, s);//确定block的形势是s*s的
 
-    FLOAT *dx = NULL, *hx = NULL;
+    FLOAT *dx = NULL, *hx = NULL;//定义host和device的内存
     FLOAT *dy = NULL, *hy = NULL;
     FLOAT *dz = NULL, *hz = NULL;
 
@@ -45,11 +45,11 @@ int main()
     double th, td;
 
     /* allocate GPU mem */
-    cudaMalloc((void **)&dx, nbytes);
-    cudaMalloc((void **)&dy, nbytes);
+    cudaMalloc((void **)&dx, nbytes);//申请内存空间，这就是申请空间的格式
+    cudaMalloc((void **)&dy, nbytes);//dy，dx，dz是申请空间的向量，nbytes是申请的大小
     cudaMalloc((void **)&dz, nbytes);
 
-    if (dx == NULL || dy == NULL || dz == NULL) {
+    if (dx == NULL || dy == NULL || dz == NULL) {//判断是否为空，确定是否运行
         printf("couldn't allocate GPU memory\n");
         return -1;
     }
@@ -57,7 +57,7 @@ int main()
     printf("allocated %.2f MB on GPU\n", nbytes / (1024.f * 1024.f));
 
     /* alllocate CPU mem */
-    hx = (FLOAT *) malloc(nbytes);
+    hx = (FLOAT *) malloc(nbytes);//标准c语言的函数，申请内存
     hy = (FLOAT *) malloc(nbytes);
     hz = (FLOAT *) malloc(nbytes);
 
@@ -69,33 +69,33 @@ int main()
 
     /* init */
     for (i = 0; i < N; i++) {
-        hx[i] = 1;
+        hx[i] = 1;//对其进行初始化
         hy[i] = 1;
         hz[i] = 1;
     }
 
     /* copy data to GPU */
-    cudaMemcpy(dx, hx, nbytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dx, hx, nbytes, cudaMemcpyHostToDevice);//（目的地，sourse来源，内存大小，方向host——device）
     cudaMemcpy(dy, hy, nbytes, cudaMemcpyHostToDevice);
     cudaMemcpy(dz, hz, nbytes, cudaMemcpyHostToDevice);
 
     /* call GPU */
-    cudaDeviceSynchronize();
-    td = get_time();
+    cudaDeviceSynchronize();//等GPU计算完CPU再运行的函数
+    td = get_time();//获取时间
     
-    for (i = 0; i < itr; i++) vec_add<<<grid, bs>>>(dx, dy, dz, N);
+    for (i = 0; i < itr; i++) vec_add<<<grid, bs>>>(dx, dy, dz, N);//运行cuda的函数
 
-    cudaDeviceSynchronize();
-    td = get_time() - td;
+    cudaDeviceSynchronize();//等GPU计算完CPU再运行的函数
+    td = get_time() - td;//计算时间
 
     /* CPU */
     th = get_time();
-    for (i = 0; i < itr; i++) vec_add_host(hx, hy, hz, N);
-    th = get_time() - th;
+    for (i = 0; i < itr; i++) vec_add_host(hx, hy, hz, N);//运行普通c语言的函数
+    th = get_time() - th;//计算时间
 
     printf("GPU time: %e, CPU time: %e, speedup: %g\n", td, th, th / td);
 
-    cudaFree(dx);
+    cudaFree(dx);//申请的内存要释放掉
     cudaFree(dy);
     cudaFree(dz);
 
